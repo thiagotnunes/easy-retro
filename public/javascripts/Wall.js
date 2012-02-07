514e5f5ac6e9f35d6bdab1813767679e4379afb6
Mauricio Sanches <mauricio.silveira.sanches@gmail.com>
2012-02-07 14:56:52 -0200

    bauricio - passing first jasmine spec.

--- public/javascripts/Wall.js
+++ public/javascripts/Wall.js
@@ -1,10 +1,10 @@
 var wall = function() {
   var addPostIt = function() {
-    var newPostIt = $("<div id='post_it_1'></div>").text($("#new_post_it_text").val());
+    var newPostIt = $("<div id='post_it_1'></div>").html($("#new_post_it_text").val());
     $("#post_its").append(newPostIt);
   };
   var bindButtons = function() {
-    $("add_post_it").click(addPostIt);
+    $("#add_post_it").click(addPostIt);
   };
   return {
     bindButtons: bindButtons
--- spec/javascripts/WallSpec.js
+++ spec/javascripts/WallSpec.js
@@ -1,9 +1,9 @@
 describe("Wall", function() {
   it("should add post it with given text", function() {
     loadFixtures("wall.html");
-    $("#new_post_it_text").text("build takes too long");
-    expect($("#post_it_1")).toHaveText("build takes too long");
-
+    wall().bindButtons();
+    $("#new_post_it_text").val("build takes too long");
+    $("#add_post_it").click();
+    expect($("#post_it_1")).toHaveHtml("build takes too long");
   });
-
 });
