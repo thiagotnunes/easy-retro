d9c1207f06d52bac7811c48f5ca529229d5f5046
Thiago Nunes <thiagotnunes@gmail.com>
2012-02-07 19:41:45 -0200

    tnunes: Adding containment to the post-it movement

--- public/javascripts/Wall.js
+++ public/javascripts/Wall.js
@@ -2,7 +2,15 @@ var wall = function() {
   var addPostIt = function() {
     var newPostIt = $("#base_post_it").clone()
       .attr("id", "new_post_it")
-      .draggable({handle: ".header"})
+      .draggable({
+        containment: "#board", 
+        handle: ".header",
+      })
+      .css({
+        position: "absolute",
+        top: "50px",
+        left: "10px"
+      })
       .show();
     $("#board").append(newPostIt);
   };
