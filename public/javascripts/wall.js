712ebdda3e7a48acd4b49f779a812a26ab7ef9bd
Joao Vortmann <jvortmann@gmail.com>
2012-02-08 19:09:18 -0200

    jvortmann: add buttons to create well, not_so_well and action_item post its.

--- public/javascripts/wall.js
+++ public/javascripts/wall.js
@@ -1,21 +1,50 @@
 var wall = function() {
-  var addPostIt = function() {
-    var newPostIt = $("#base_post_it").clone()
-      .attr("id", "post_it")
-      .removeClass("hidden")
-      .addClass("new")
-      .show();
-    $("#board").append(newPostIt);
-    newPostIt.draggable({
-        containment: "#board", 
-        handle: ".header",
-      });
+  var createPostIt = function(type) {
+    var postIt = $("#base_post_it").clone()
+          .attr("id", "post_it")
+          .removeClass("hidden")
+          .addClass("new")
+          .addClass(type)
+          .show();
 
+    return postIt;
+  }
+
+  var appendPostItToBoard = function(postIt) {
+    $("#board").append(postIt);
+  }
+
+  var makePostItDraggable = function(postIt){
+    postIt.draggable({
+      containment: "#board", 
+      handle: ".header",
+    });
+  }
+
+  var wellPostIt = function() {
+    var postIt = createPostIt("well");
+    appendPostItToBoard(postIt);
+    makePostItDraggable(postIt);
+  };
+
+  var notSoWellPostIt = function() {
+    var postIt = createPostIt("not_so_well");
+    appendPostItToBoard(postIt);
+    makePostItDraggable(postIt);
+  };
+
+  var actionItemPostIt = function() {
+    var postIt = createPostIt("action_item");
+    appendPostItToBoard(postIt);
+    makePostItDraggable(postIt);
   };
 
   var bindButtons = function() {
-    $("#add_well").click(addPostIt);
+    $("#add_well").click(wellPostIt);
+    $("#add_not_so_well").click(notSoWellPostIt);
+    $("#add_action_item").click(actionItemPostIt);
   };
+
   return {
     bindButtons: bindButtons
   }
