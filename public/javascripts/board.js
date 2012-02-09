0cc57909dff4f01f4856bfa20c01f6bf63c108c9
Joao Vortmann <jvortmann@gmail.com>
2012-02-09 16:26:49 -0200

    jvortmann: making the bad postit work.

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -16,6 +16,10 @@ var board = function() {
     easyRetro.create({group: "not_so_well"});
   };
 
+  var badPostIt = function() {
+    easyRetro.create({group: "bad"});
+  };
+
   var actionItemPostIt = function() {
     easyRetro.create({group: "action_item"});
   };
@@ -27,6 +31,7 @@ var board = function() {
   var bindButtons = function() {
     $("#add_well").click(publishPostIt);
     $("#add_not_so_well").click(notSoWellPostIt);
+    $("#add_bad").click(badPostIt);
     $("#add_action_item").click(actionItemPostIt);
   };
 
