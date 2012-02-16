c1c14bffa2709442dfb574e97359c2a942766888
Thiago Nunes <thiagotnunes@gmail.com>
2012-02-16 19:56:56 -0200

    thiagotnunes: fixing the case where the board has no post it

--- lib/board_listener.rb
+++ lib/board_listener.rb
@@ -11,6 +11,7 @@ class BoardListener
       name = message["data"]["board"]
       postIt = message["data"]["postIt"]
       board = @boards.find_one({name: name})
+      board["postIts"] = {} if board["postIts"].nil?
       board["postIts"][postIt["id"]] = postIt
 
       @boards.update({name: name}, board)
