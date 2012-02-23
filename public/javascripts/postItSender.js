7b17ce1405de20f7e2450f322d570d0f746f5860
Thiago Nunes <thiagotnunes@gmail.com>
2012-02-23 19:20:56 -0200

    Undoing some crap I did

--- public/javascripts/postItSender.js
+++ public/javascripts/postItSender.js
@@ -6,9 +6,7 @@ var postItSender = function() {
   };
 
   var send = function(postIt) {
-    var publication = client.publish("/board", postIt);
-    publication.callback(function() { console.log('Success'); });
-    publication.errback(function(error) { console.log(error); });
+    client.publish("/board", postIt);
   };
 
   return {
