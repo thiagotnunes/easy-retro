a0ff6e44b091ba8508354d373f3110f311865f49
Mauricio Sanches <mauricio.silveira.sanches@gmail.com>
2012-03-02 09:34:37 -0300

    Starting to refactor board listener code, moving logic from it.

--- spec/board_listener_spec.rb
+++ spec/board_listener_spec.rb
@@ -9,62 +9,12 @@ describe BoardListener, do
     @listener = BoardListener.new(@repo)
   end
 
-  it "should add new post it for a board without post its" do
-    message = {
-      "channel" => "/board",
-      "data" => {
-        "action" => "create",
-        "board" => {
-          "name" => "theBoard",
-          "postIt" => {
-            "id" => "1",
-            "text" => "other text"
-          }
-        }
-      }
-    }
-
-   @repo.should_receive(:get).with("theBoard").and_return({})
-   @repo.should_receive(:update).with({"postIts" => {"1" => {"id" => "1", "text" => "other text"}}})
-   @callback.should_receive(:call).with(message) 
-
-   @listener.outgoing message, @callback
-  end
-
-  it "should update post it for a board with the given post it" do
-    message = {
-      "channel" => "/board",
-      "data" => {
-        "action" => "create",
-        "board" => {
-          "name" => "theBoard",
-          "postIt" => {
-            "id" => "1",
-            "text" => "other text"
-          }
-        }
-      }
-    }
-
-   @repo.should_receive(:get).with("theBoard").and_return({"postIts" => {"1" => {"id" => "1", "text" => "some text"}}})
-   @repo.should_receive(:update).with({"postIts" => {"1" => {"id" => "1", "text" => "other text"}}})
-   @callback.should_receive(:call).with(message) 
-
-   @listener.outgoing message, @callback
-  end
-
   it "remove a post it" do
     message = {
       "channel" => "/board",
       "data" => {
         "action" => "remove",
-        "board" => {
-          "name" => "theBoard",
-          "postIt" => {
-            "id" => "1",
-            "text" => "other text"
-          }
-        }
+        "board" => { "name" => "theBoard", "postIt" => { "id" => "1", "text" => "other text" } }
       }
     }
 
