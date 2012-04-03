dc365b9e8dd08e1e1e405ac69d0a6d68e040d07d
Joao Vortmann <jvortmann@gmail.com>
2012-04-03 11:13:16 -0300

    jvortmann: setting the local database name only for development.

--- init/mongo.rb
+++ init/mongo.rb
@@ -2,4 +2,6 @@ require 'mongo_mapper'
 
 MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOLAB_URI']) if ENV['MONGOLAB_URI']
 
-MongoMapper.database = "easy_retro"
+configure :development do
+  MongoMapper.database = "easy_retro"
+end
