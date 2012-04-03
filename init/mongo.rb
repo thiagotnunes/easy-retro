27ca35337f03a176eb21233bf00a304366bc3b53
Joao Vortmann <jvortmann@gmail.com>
2012-04-03 11:50:30 -0300

    jvortmann: fixing environment variable name DUH.

--- init/mongo.rb
+++ init/mongo.rb
@@ -1,8 +1,9 @@
 require 'mongo_mapper'
-require 'uri'
 
 configure :production do
-  uri = URI.parse(ENV['MONGOHQ_URL'])
+  require 'uri'
+
+  uri = URI.parse(ENV['MONGOLAB_URI'])
   MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
   MongoMapper.database = uri.path.gsub(/^\//, '')
 end
