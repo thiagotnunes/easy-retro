e20bcc244acb9679def7c3dcd17e8cb2e85bec6c
Joao Vortmann <jvortmann@gmail.com>
2012-04-03 11:46:53 -0300

    jvortmann: setting up configuration for production.

--- init/mongo.rb
+++ init/mongo.rb
@@ -1,6 +1,11 @@
 require 'mongo_mapper'
+require 'uri'
 
-MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOLAB_URI']) if ENV['MONGOLAB_URI']
+configure :production do
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+  MongoMapper.database = uri.path.gsub(/^\//, '')
+end
 
 configure :development do
   MongoMapper.database = "easy_retro"
